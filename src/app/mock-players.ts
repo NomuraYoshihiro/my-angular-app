5fbff69e2e76bb46e1db4ac7dd721b97358b17d3
nomuyoshi <nomuyoshi0111@gmail.com>
2018-04-22 22:42:45 +0900

    mock作成

---
+++ src/app/mock-players.ts
@@ -0,0 +1,9 @@
+import { Player } from './player';
+
+export const PLAYERS: Player[] = [
+  { id: 1, name: '香川真司', age: 29, position: 'MF' },
+  { id: 2, name: '中島翔哉', age: 23, position: 'FW' },
+  { id: 3, name: '酒井宏樹', age: 29, position: 'DF' },
+  { id: 4, name: '吉田麻也', age: 29, position: 'DF' },
+  { id: 5, name: '遠藤保仁', age: 38, position: 'MF' },
+];
