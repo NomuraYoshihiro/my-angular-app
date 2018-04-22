e5ab5e9baa2343814fd46a37d455e22079bcd6b7
nomuyoshi <nomuyoshi0111@gmail.com>
2018-04-22 13:45:00 +0900

    basic form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BasicFormComponent } from './basic-form/basic-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicFormComponent
   ],
   imports: [
     BrowserModule
